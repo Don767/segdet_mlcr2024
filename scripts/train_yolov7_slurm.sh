9f69e1c8514e437379c751bd9053347bb16d5363
William Guimont-Martin <wil9876543210@hotmail.com>
2024-04-17 08:22:51 -0400

    batch size 64

--- scripts/train_yolov7_slurm.sh
+++ scripts/train_yolov7_slurm.sh
@@ -14,7 +14,7 @@ docker run --gpus all -e CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES --rm --ipc h
   --mount type=bind,source=.,target=/app/ \
   --mount type=bind,source=$(pwd)/data/coco,target=/app/data/coco \
   --mount type=bind,source=/dev/shm,target=/dev/shm \
-  segdet bash -c "mim install mmcv==2.1.0 mmdet==3.3.0 && python3 segdet/models/yolov7/yolo_train.py --workers 8 --device $CUDA_VISIBLE_DEVICES --batch-size 128 --data config/coco.yaml --img 640 640 --cfg config/config_yolov7.yaml --weights yolov7_training.pt --name yolov7-base --hyp config/hyp_scratch_yolov7.yaml"
+  segdet bash -c "mim install mmcv==2.1.0 mmdet==3.3.0 && python3 segdet/models/yolov7/yolo_train.py --workers 8 --device $CUDA_VISIBLE_DEVICES --batch-size 64 --data config/coco.yaml --img 640 640 --cfg config/config_yolov7.yaml --weights yolov7_training.pt --name yolov7-base --hyp config/hyp_scratch_yolov7.yaml"
 
 #--workers 8 --device 0 --batch-size 32 --data ../../config/coco.yaml --img 640 640 --cfg ../../config/config_yolov7.yaml --weights 'yolov7_training.pt' --name yolov7-base --hyp ../../config/hyp_scratch_yolov7.yaml
 
