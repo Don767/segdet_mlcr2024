14e52c2b718ed861f9cec7ecf887f85be25a2415
William Guimont-Martin <wil9876543210@hotmail.com>
2024-04-21 00:09:43 -0400

    try to set GPU number

--- scripts/train_slurm.sh
+++ scripts/train_slurm.sh
@@ -9,12 +9,15 @@ cd ~/segdet_mlcr2024 || exit
 docker build -t segdet .
 
 echo "Running on GPU $CUDA_VISIBLE_DEVICES"
+echo "mim install mmcv==2.1.0 mmdet==3.3.0 && CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES python3 tools/train.py $CONFIG --gpu-ids $CUDA_VISIBLE_DEVICES" > $NAME.sh
+chmod +x $NAME.sh
+
 # To debug and get the logs directly in the slurm output
 docker run --gpus all -e CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES -e GPU=$CUDA_VISIBLE_DEVICES --rm --ipc host \
   --mount type=bind,source=.,target=/app/ \
   --mount type=bind,source=$(pwd)/data/coco,target=/app/data/coco \
   --mount type=bind,source=/dev/shm,target=/dev/shm \
-  segdet bash -c "mim install mmcv==2.1.0 mmdet==3.3.0 && CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES python3 tools/train.py $CONFIG --gpu-ids $CUDA_VISIBLE_DEVICES"
+  segdet ./$NAME.sh
 
 #container_id=$(
 #  docker run --detach --gpus all -e CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES --rm --ipc host \
