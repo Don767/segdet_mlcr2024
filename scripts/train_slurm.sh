f38ad474efe98caa8f364adec2e6dfa88c3b0577
William Guimont-Martin <wil9876543210@hotmail.com>
2024-04-21 08:57:37 -0400

    wip

--- scripts/train_slurm.sh
+++ scripts/train_slurm.sh
@@ -9,15 +9,21 @@ cd ~/segdet_mlcr2024 || exit
 docker build -t segdet .
 
 echo "Running on GPU $CUDA_VISIBLE_DEVICES"
-echo "export CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES && mim install mmcv==2.1.0 mmdet==3.3.0 && python3 tools/train.py $CONFIG" > $NAME.sh
-chmod +x $NAME.sh
+#echo "mim install mmcv==2.1.0 mmdet==3.3.0 && python3 tools/train.py $CONFIG" > $NAME.sh
+#chmod +x $NAME.sh
 
 # To debug and get the logs directly in the slurm output
-docker run --gpus \"device=${CUDA_VISIBLE_DEVICES}\" -e CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES -e GPU=$CUDA_VISIBLE_DEVICES --rm --ipc host \
+docker run --gpus \"device=${CUDA_VISIBLE_DEVICES}\" --rm --ipc host \
   --mount type=bind,source=.,target=/app/ \
   --mount type=bind,source=$(pwd)/data/coco,target=/app/data/coco \
   --mount type=bind,source=/dev/shm,target=/dev/shm \
-  segdet ./$NAME.sh
+  segdet bash -c "mim install mmcv==2.1.0 mmdet==3.3.0 && python3 tools/train.py $CONFIG"
+
+#docker run --gpus \"device=${CUDA_VISIBLE_DEVICES}\" -e CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES -e GPU=$CUDA_VISIBLE_DEVICES --rm --ipc host \
+#  --mount type=bind,source=.,target=/app/ \
+#  --mount type=bind,source=$(pwd)/data/coco,target=/app/data/coco \
+#  --mount type=bind,source=/dev/shm,target=/dev/shm \
+#  segdet ./$NAME.sh
 
 #container_id=$(
 #  docker run --detach --gpus all -e CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES --rm --ipc host \
